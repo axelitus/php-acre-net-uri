d7f63d74e5da25aa6b862120b3506dc15024233e
Axel Pardemann <axelitus2005@gmail.com>
2012-08-24 23:24:47 -0500

    Added some Uri functionality

--- classes/axelitus/Acre/Net/Uri/Uri.php
+++ classes/axelitus/Acre/Net/Uri/Uri.php
@@ -12,6 +12,8 @@
 
 namespace axelitus\Acre\Net\Uri;
 
+use InvalidArgumentException;
+
 /**
  * Uri Class
  *
@@ -24,7 +26,17 @@ class Uri
 {
     const SCHEME_SEPARATOR = ':';
     const FRAGMENT_SEPARATOR = '#';
-    const REGEX = "/^(?#scheme)(?:(?P<scheme>[A-Za-z][A-Za-z0-9+\-.]*):)?(?:(?#authority)//(?#userinfo)(?:(?P<userinfo>.+)@)?(?#host)(?P<host>(?:(?#named|IPv4)[A-Za-z0-9\-._~%]+|(?#IPv6)\[[A-Fa-f0-9:.]+\]|(?#IPvFuture)\[v[A-Fa-f0-9][A-Za-z0-9\-._~%!$&'()*+,;=:]+\])?)(?#port)(?::(?P<port>[0-9]+))?)?(?#path)(?:/?(?P<path>(?:[A-Za-z0-9\-._~%!$&'()*+,;=@]+/?)*))?(?#query)(?:\?(?P<query>[A-Za-z0-9\-._~%!$&'()*+,;=:@/?]*))?(?#fragment)(?:\#(?P<fragment>[A-Za-z0-9\-._~%!$&'()*+,;=:@/?]*))?$/";
+    const REGEX = <<<REGEX
+/^(?#scheme)(?:(?P<scheme>[A-Za-z][A-Za-z0-9+\-.]*):)?
+(?:(?#authority)\/\/(?P<authority>
+  (?#userinfo)(?:(?P<userinfo>.+)@)?
+  (?#host)(?P<host>(?:(?#named|IPv4)[A-Za-z0-9\-._~%]+|(?#IPv6)\[[A-Fa-f0-9:.]+\]|(?#IPvFuture)\[v[A-Fa-f0-9][A-Za-z0-9\-._~%!$&\'()*+,;=:]+\])?)
+  (?#port)(?::(?P<port>[0-9]+))?
+))?
+(?#path)(?:\/?(?P<path>(?:[A-Za-z0-9\-._~%!$&\'()*+,;=@]+\/?)*))?
+(?#query)(?:\?(?P<query>[A-Za-z0-9\-._~%!$&\'()*+,;=:@\/?]*))?
+(?#fragment)(?:\#(?P<fragment>[A-Za-z0-9\-._~%!$&\'()*+,;=:@\/?]*))?$/x
+REGEX;
 
     protected $_scheme = '';
     protected $_authority = null;
@@ -32,14 +44,65 @@ class Uri
     protected $_query = null;
     protected $_fragment = '';
 
-    public function parse($uri)
+    protected function __construct($components = '')
     {
+        $components = is_string($components) ? static::parse($components) : $components;
+        $this->_scheme = $components['scheme'];
+        $this->_authority = (is_array($components['authority'])) ? Authority::forge($components['authority'])
+            : Authority::parse($components['authority']);
+        Authority::parse($components['authority']);
+        $this->_path = Path::forge($components['path']);
+        $this->_query = Query::forge($components['query']);
+        $this->_fragment = $components['fragment'];
+    }
 
+    public static function forge($components = '')
+    {
+        if (!is_string($components) or !is_array($components)) {
+            throw new InvalidArgumentException("The \$components parameter must be a string or an array.");
+        }
+
+        return new static($components);
+    }
+
+    public static function parse($uri)
+    {
+        if (!is_string($uri)) {
+            throw new InvalidArgumentException("The \$uri parameter must be a string.");
+        }
+
+        preg_match(static::REGEX, $uri, $matches);
+
+        $components = array(
+            'scheme'    => $matches['scheme'],
+            'authority' => $matches['authority'],
+            'path'      => $matches['path'],
+            'query'     => $matches['query'],
+            'fragment'  => $matches['fragment']
+        );
+
+        return $components;
     }
 
     public static function validate($uri)
     {
+        return (bool)preg_match(static::REGEX, $uri);
+    }
 
+    protected function build()
+    {
+        $uri = $this->_scheme;
+        $uri .= (string)$this->_authority;
+        $uri .= (string)$this->_path;
+        $uri .= (string)$this->_query;
+        $uri .= ($this->_fragment != '') ? '#'.$this->_fragment : '';
+
+        return $uri;
+    }
+
+    public function __toString()
+    {
+        return $this->build();
     }
 
     public function isValid()
