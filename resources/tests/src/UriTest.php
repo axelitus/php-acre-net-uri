f214e6d748322edd8456575af2fa0bdc51b44023
Axel Pardemann <axelitus2005@gmail.com>
2012-11-12 14:37:40 -0600

    Added some more tests

--- resources/tests/src/UriTest.php
+++ resources/tests/src/UriTest.php
@@ -64,7 +64,94 @@ class UriTest extends \PHPUnit_Framework_TestCase
      */
     public function testUriValidate()
     {
+        // *** Absolute URIs ***
+        // Example URIS taken from: http://en.wikipedia.org/wiki/Uniform_resource_identifier#Examples_of_absolute_URIs
+        $uri = 'http://example.org/absolute/URI/with/absolute/path/to/resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
 
+        $uri = 'ftp://example.org/resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = 'urn:issn:1535-3613';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        // *** URI references
+        // Example URIS taken from: http://en.wikipedia.org/wiki/Uniform_resource_identifier#Examples_of_URI_references
+        $uri = 'http://en.wikipedia.org/wiki/URI#Examples_of_URI_references';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = 'http://example.org/absolute/URI/with/absolute/path/to/resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = '//example.org/scheme-relative/URI/with/absolute/path/to/resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = '/relative/URI/with/absolute/path/to/resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = 'relative/path/to/resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = '../../../resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = './resource.txt#frag01';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = 'resource.txt';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = '#frag01';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+
+        $uri = '';
+        $output = Uri::validate($uri);
+        $this->assertTrue($output);
+    }
+
+    /**
+     * Tests Uri::validate() with matches
+     *
+     * @test
+     */
+    public function testUriValidateMatch()
+    {
+        $uri = 'http://en.wikipedia.org/wiki/URI#Examples_of_URI_references';
+        $output = Uri::validate($uri, $matches);
+        $this->assertTrue($output);
+
+        $expected = 'http';
+        $this->assertEquals($expected, $matches['scheme']);
+
+        $expected = 'en.wikipedia.org';
+        $this->assertEquals($expected, $matches['authority']);
+
+        $expected = '';
+        $this->assertEquals($expected, $matches['userinfo']);
+
+        $expected = 'en.wikipedia.org';
+        $this->assertEquals($expected, $matches['host']);
+
+        $expected = '';
+        $this->assertEquals($expected, $matches['port']);
+
+        $expected = '/wiki/URI';
+        $this->assertEquals($expected, $matches['path']);
+
+        $expected = 'Examples_of_URI_references';
+        $this->assertEquals($expected, $matches['fragment']);
     }
 
     /**
